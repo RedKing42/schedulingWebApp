4035e82c49dd2d74010c7238695c219bf8afa6a9
ro_boat <rory.wagner@dixiesuccess.org>
2019-08-29 00:03:42 -0600

    Still working on a couple of things...

--- calendar.js
+++ calendar.js
@@ -1,8 +1,47 @@
-int; numOfWorkHours = 10
-int; numOfDays = 7
 
-for(int; i = 0; i < numOfDays){
-    for(int; j = 0; j < numOfWorkHours){
-        document.write(i, " ", j)
+
+var numOfWorkHours = 10
+var numOfDays = 7
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+class calendar{
+
+    constructor(){
+        events = []
+        this.initializeEvents()
     }
+
+    initializeEvents(){
+        for(int; i = 0; i < numOfDays){
+            for(int; j = 0; j < numOfWorkHours){
+                events[j * numOfDays + i] = ""
+            }
+        }
+    }
+
+    retrieveEvent(i, j){
+        if (i < numOfDays) and (i >= 0); {
+            if( j < numOfWorkHours) and (j >=0); {
+                return events[j * numOfDays + i]
+            }
+        }
+    }
+
+    changeEvent(i, j, newEvent){
+        if (i < numOfDays) and (i >= 0); {
+            if( j < numOfWorkHours) and (j >=0); {
+                events[j * numOfDays + i] = newEvent
+            }
+        }
+    }
+}
+
+function main(){
+    var mainCalendar = calendar()
+
+    mainCalendar.printCalendar()
+}
+
+function displayCalendar(mainCalendar){
+
 }
